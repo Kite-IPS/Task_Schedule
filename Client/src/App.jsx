ddc2d2359f1505fed6757f0971aed4a905d8b2ff
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2025-10-11 13:21:11 +0530

    Route setup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Pages/Login';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </Router>
+
   )
 }
 
