aba1fc86d52a4bf1eca4074a0331e9d4c9f92254
Sivakanesh <sksivakanesh12@gmail.com>
2025-10-13 14:57:28 +0530

    adding roles

--- backend/task/models.py
+++ backend/task/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 class Staff(models.Model):
+    class Roles(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        STAFF = 'STAFF', 'Staff'
+        HOD = 'HOD', 'Head of Department'
+    
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     department = models.CharField(max_length=100)
-    role = models.CharField(max_length=50)     
+    role = models.CharField(
+        max_length=10,
+        choices=Roles.choices,
+        default=Roles.STAFF
+    )
     password = models.CharField(max_length=128)
 
 
